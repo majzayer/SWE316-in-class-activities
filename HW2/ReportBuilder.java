5b7592da7da7839710f748b5d5a82442f43b7f94
Majed Al Zayer <majed.zayer@gmail.com>
2024-11-04 16:22:10 +0300

    Create ReportBuilder.java

---
+++ HW2/ReportBuilder.java
@@ -0,0 +1,28 @@
+public class ReportBuilder {
+
+    public String buildReport(Employee employee) {
+        StringBuilder report = new StringBuilder();
+        
+        // Add header, main content, and footer
+        report.append(addReportHeader());
+        report.append(createMainReportContent(employee));
+        report.append(addReportFooter());
+        
+        return report.toString();
+    }
+
+    // Default implementation for header
+    public String addReportHeader() {
+        return "=== Report Header ===\n";
+    }
+
+    public String createMainReportContent(Employee employee) {
+        MainReportContent contentProvider = new MainReportContent();
+        return contentProvider.makeMainReportContent(employee);
+    }
+
+    // Default implementation for footer
+    public String addReportFooter() {
+        return "\n=== Report Footer ===";
+    }
+}
